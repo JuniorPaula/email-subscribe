e0442d13ae6c39652806e7a71ca925acb6707e40
Junior Paula <luke.junnior@icloud.com>
2023-05-11 20:14:20 -0300

    Create method to connect postgres database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/joho/godotenv"
+)
+
 const webPort = "80"
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error to load .env file")
+	}
+
 	// connect to the database
+	db := initDB()
+	db.Ping()
 
 	// create sessions
 
@@ -16,4 +35,51 @@ func main() {
 	// set up mail
 
 	// listen for web connections
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("can't connect to database")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dns := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dns)
+		if err != nil {
+			log.Println("postgres not yet ready...")
+		} else {
+			log.Println("connected to database!")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for 1 second")
+		time.Sleep(1 * time.Second)
+		counts++
+
+		continue
+	}
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dns)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
